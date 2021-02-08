13c7c148a042290b8aa4545e76d198959009d683
altafmhss <36732944+altafmhss@users.noreply.github.com>
2021-02-08 15:31:11 +0530

    Add files via upload

---
+++ src/main/java/com/svscorp/svscorp/sec/ff.java
@@ -0,0 +1,19 @@
+package com.svscorp.svscorp.sec;
+
+import com.azure.spring.aad.webapp.AADWebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ff extends AADWebSecurityConfigurerAdapter {
+
+    /**
+     * Add configuration logic as needed.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
